e238ec284a1860654b1bd58a04b46a6e2ddc9099
Jonas Lackmann <lackmann.jonas@gmail.com>
2020-03-12 17:00:32 +0100

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,13 @@
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log("private route hit!");
+		next();
+	}, 
+	logger: function (req, res, next) {
+		var date = new Date().toString();
+		console.log("Request: " + req.method + " " + req.originalURL + " Zeit: " + date);
+		next();
+	}
+};
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -2,26 +2,14 @@ var express = require("express");
 var app = express();
 const PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function(req, res, next) {
-		console.log("private route hit!");
-		next();
-	}, 
-	logger: function (req, res, next) {
-		var date = new Date().toString();
-		console.log("Request: " + req.method + " " + req.originalURL + " Zeit: " + date);
-		next();
-	}
-};
+var middleware = require("./middleware.js");
 
-//app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 
 app.get("/about", middleware.requireAuthentication, function (req, res) {
 	res.send("Hi, I´m Jones! What´s up?");
 });
 
-//app.use(express.static());
 console.log(__dirname);
 
 app.use(express.static(__dirname + "/public"));
